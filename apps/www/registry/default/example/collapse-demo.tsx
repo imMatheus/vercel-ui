b3f7b1fbc95dcc9a8346eb29689ad606404f370f
imMatheus <matheusmendesbarata03@gmail.com>
2024-03-17 13:35:52 +0100

    did stuff

--- apps/www/registry/default/example/collapse-demo.tsx
+++ apps/www/registry/default/example/collapse-demo.tsx
@@ -2,12 +2,21 @@ import { Collapse, CollapseGroup } from "@/registry/default/ui/collapse"
 
 export default function CollapseDemo() {
   return (
-    <CollapseGroup type="single" className="w-full bg-red-500">
+    <CollapseGroup>
       <Collapse value="item-1" title="Question A">
-        <p>geh</p>
+        {/* TODO add the text component */}
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
       </Collapse>
       <Collapse value="item-2" title="Question B">
-        <p>geh</p>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur.
+        </p>
       </Collapse>
     </CollapseGroup>
   )
