f5000c8323bd027fdac2ea0388e1d10691e2060a
imMatheus <matheusmendesbarata03@gmail.com>
2024-04-14 18:51:51 +0200

    Refactor menu link items

--- apps/www/registry/default/example/menu-link-items.tsx
+++ apps/www/registry/default/example/menu-link-items.tsx
@@ -7,19 +7,13 @@ import {
 
 export default function MenuLinksDemo() {
   return (
-    <>
-      <MenuContainer>
-        <MenuButton>Actions</MenuButton>
-        <Menu width={200}>
-          <MenuLink href="/design/menu#custom-trigger">One</MenuLink>
-          <MenuLink href="#">Two</MenuLink>
-          <MenuLink href="#">Three</MenuLink>
-        </Menu>
-      </MenuContainer>
-      <div className="group">
-        <p className="bg-red-400">class</p>
-        <p className="!no-underline">heej</p>
-      </div>
-    </>
+    <MenuContainer>
+      <MenuButton>Actions</MenuButton>
+      <Menu width={200}>
+        <MenuLink href="/design/menu#custom-trigger">One</MenuLink>
+        <MenuLink href="#">Two</MenuLink>
+        <MenuLink href="#">Three</MenuLink>
+      </Menu>
+    </MenuContainer>
   )
 }
